ed9dc6e88104b7ea073e1dea4a7ca0e3e1b924c9
damianryann <damian@8mountains.co.uk>
2021-05-31 12:00:40 +0100

    fixed syntax and added new variations and fuck yous

--- shorsey.js
+++ shorsey.js
@@ -22,8 +22,10 @@ const shuffle = a => {
 client.on('message', msg => {
   const fuckResponses = shuffle([
     `Fuck you ${msg.author} your mom keeps tryin' to slip a finger in my bum but I keep telling her that I only let <@!665683939410706442>'s mom do that ya fuckin loser`,
+    `Fuck you, ${msg.author}, your mom molested me two Halloweens ago, shut the fuck up or I’m taking it to Twitter.`,
     `Fuck you ${msg.author}, I made your mom cum so hard that they made a canadian heritage moment out of it and Don Mckellar played my dick`,
     `Fuck you ${msg.author} your mom shot cum straight across the room and killed my siamese fighting fish, threw off the PH levels in my aquarium`,
+    `Fuck you, ${msg.author}. Tell your mom to top up the cell phone she bought me so I can FaceTime her late night.`,
     `Fuck you ${msg.author}. Your breath's so bad it gave me an existential crisis it made me question my whole life`,
     `Fuck you ${msg.author}. Your mom loves butt play the way I love Haagen daz. Let's go get some fucking ice cream`,
     `Fuck you ${msg.author}, fight me see what happens`,
@@ -42,16 +44,37 @@ client.on('message', msg => {
     `${msg.author} Your life's so sad I get a charity tax break just for hanging out with you`,
     `${msg.author} Nice sweep. No sweep. Give your balls a tug`
   ]);
+
+  const comingResponse = shuffle([
+    `${msg.author} Heard the same thing, bud, from your mom seven times, and that’s not even my record, ya fucking loser.`
+  ]);
+
+  const whatHappens = shuffle([
+    'Three things: I hit you, you hit the pavement, ambulance hits 60.',
+    'Three things: I hit you, you hit the pavement, and I j*rk off on your driver’s side door handle.'
+  ]);
+
   const response = msg.content.toLowerCase().includes('fuck you')
     ? fuckResponses[Math.floor(Math.random() * fuckResponses.length)]
     : responses[Math.floor(Math.random() * responses.length)];
 
-  if (msg.author.bot) return false;
-
-  if (msg.content.includes('@here') || msg.content.includes('@everyone'))
+  if (msg.author.bot) {
     return false;
+  }
+
+  if (msg.content.includes('@here') || msg.content.includes('@everyone')) {
+    return false;
+  }
 
   if (msg.mentions.has(client.user.id)) {
     msg.channel.send(response);
+  }
+
+  if (msg.content === `I'm coming`) {
+    msg.channel.send(comingResponse);
+  }
+
+  if (msg.content === `What's going to happen?`) {
+    msg.channel.send(comingResponse);
   }
 });
